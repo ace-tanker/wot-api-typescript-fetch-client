7eb923a872d02d3d269fe31f3ee0c41da070ad79
Adrien Simonnet <56685429+adrien-simonnet@users.noreply.github.com>
2025-07-01 16:24:16 +0200

    Generated version 0.0.0

---
+++ src/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountsApi.js';
+export * from './AuthenticationApi.js';
+export * from './ClanRatingsApi.js';
+export * from './ClansApi.js';
+export * from './GlobalMapApi.js';
+export * from './PlayersVehiclesApi.js';
+export * from './StrongholdsApi.js';
+export * from './TankopediaApi.js';
